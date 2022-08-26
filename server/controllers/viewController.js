dadaeec10786a8b277a988730580a563fa15e9b1
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2022-08-25 21:15:26 -0400

    css look and feel

--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -1,7 +1,27 @@
 let homeView = (req, res) => {
     res.render('home')
 }
+let aboutView = (req, res) => {
+    res.render('about')
+}
+let eventsView = (req, res) => {
+    res.render('events')
+}
+let mapsView = (req, res) => {
+    res.render('maps')
+}
+let loginView = (req, res) => {
+    res.render('login')
+}
+let forumsView = (req, res) => {
+    res.render('forums')
+}
 
 module.exports = {
-    homeView: homeView
+    homeView: homeView,
+    aboutView: aboutView,
+    eventsView: eventsView,
+    mapsView: mapsView,
+    loginView: loginView,
+    forumsView: forumsView,
 }
--- server/router/router.js
+++ server/router/router.js
@@ -6,6 +6,11 @@ const router = express.Router();
 const initWebRoutes = (app) => {
 
     router.get('/', viewController.homeView);
+    router.get('/about', viewController.aboutView);
+    router.get('/events', viewController.eventsView);
+    router.get('/maps', viewController.mapsView);
+    router.get('/login', viewController.loginView);
+    router.get('/forums', viewController.forumsView);
 
     return app.use('/', router)
 };
