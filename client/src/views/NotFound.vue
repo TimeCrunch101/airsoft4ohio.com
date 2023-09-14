41b43849a4f980e23b42eb8750ff47e1d35e0d34
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-09-13 23:21:16 -0400

    404 error handle

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,4 +1,15 @@
+<script setup>
+</script>
 <template>
-    <h1>Error: Route Not Defined</h1>
-    <router-link to="/">Go Back</router-link>
+    <img class="notfound-img" src="/imgs/404.webp" alt="ERROR: 404 Page Not Found" height="400" width="550">
 </template>
+<style scoped>
+.notfound-img {
+    border: 5px solid black;
+    border-radius: 1rem;
+    margin-top: 4rem;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
