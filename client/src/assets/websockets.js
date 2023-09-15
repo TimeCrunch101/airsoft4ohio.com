768261af5d0026d17b598fa6f209d0e7905d8275
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-09-14 23:01:12 -0400

    that was annoying...

--- client/src/assets/websockets.js
+++ client/src/assets/websockets.js
@@ -3,7 +3,7 @@ import {io} from "socket.io-client";
 let socket;
 
 if (process.env.NODE_ENV === "production") {
-    socket = io("wss://a40.cincitechlabs.com:8443")
+    socket = io("wss://a4o.cincitechlabs.com:8443")
 } else {
     socket = io("ws://localhost:8443")
 }
