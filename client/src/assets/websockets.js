4e1a2a50ac4fa40725795366f65d30cf0cc0828e
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-09-14 22:58:53 -0400

    update websocket address

--- client/src/assets/websockets.js
+++ client/src/assets/websockets.js
@@ -3,7 +3,7 @@ import {io} from "socket.io-client";
 let socket;
 
 if (process.env.NODE_ENV === "production") {
-    socket = io("wss://airsoft4ohio.com:8443")
+    socket = io("wss://a40.cincitechlabs.com:8443")
 } else {
     socket = io("ws://localhost:8443")
 }
