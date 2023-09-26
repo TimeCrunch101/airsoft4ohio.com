1804e1b5c0a01bd457d7307334b04884287e9977
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-09-25 21:46:34 -0400

    confirm disable mfa before execute

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -87,20 +87,22 @@ const validateTotp = () => {
 }
 
 const disableMFA = () => {
-    set.loading = true
-    axios.patch("/api/disable/mfa", {}, {
-        headers: {
-            Authorization: `Bearer ${token.value}`
-        }
-    }).then((res) => {
-        alert("MFA Disabled")
-        getUserProfile()
-    }).catch((err) => {
-        console.error(err.response.data)
-        alert("Could not disable MFA")
-    }).finally(() => {
-        set.loading = false
-    })
+    if (confirm("Are you sure you want to disable MFA? This will make your account unsecure.")) {
+        set.loading = true
+        axios.patch("/api/disable/mfa", {}, {
+            headers: {
+                Authorization: `Bearer ${token.value}`
+            }
+        }).then((res) => {
+            alert("MFA Disabled")
+            getUserProfile()
+        }).catch((err) => {
+            console.error(err.response.data)
+            alert("Could not disable MFA")
+        }).finally(() => {
+            set.loading = false
+        })
+    }
 }
 
 const purgeAccount = () => {
@@ -121,6 +123,8 @@ const purgeAccount = () => {
             set.loading = false
             location.reload()
         })
+    } else {
+        set.loading = false
     }
 }
 
@@ -131,19 +135,36 @@ getUserProfile()
 <template>
 <Loading :loading="loading"/>
 
-<div v-if="user" class="container">
-    <p>{{ user.username }}</p>
-    <p>{{ user.email }}</p>
-    <button class="btn btn-outline-success" v-if="user.mfa_enforced !== 1" @click="enrollMFA()">Enroll MFA</button>
-    <button class="btn btn-outline-danger" @click="disableMFA()" v-else>Disable MFA</button>
-    <div v-if="mfaData.qrcode">
-        <img :src="mfaData.qrcode" alt="QRCODE">
-        <p>{{ mfaData.secret }}</p>
-        <input type="text" name="totp" id="totp" v-model="testTotp">
-        <button @click="validateTotp()">Submit TOTP Validation</button>
+
+<div class="container">
+    <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Main</button>
+            <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Security</button>
+        </div>
+    </nav>
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+            <div v-if="user" class="container">
+                <p>{{ user.username }}</p>
+                <p>{{ user.email }}</p>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+            <div v-if="user" class="container">
+                <button class="btn btn-outline-success" v-if="user.mfa_enforced !== 1" @click="enrollMFA()">Enroll MFA</button>
+                <button class="btn btn-outline-danger" @click="disableMFA()" v-else>Disable MFA</button>
+                <div v-if="mfaData.qrcode">
+                    <img :src="mfaData.qrcode" alt="QRCODE">
+                    <p>{{ mfaData.secret }}</p>
+                    <input type="text" name="totp" id="totp" v-model="testTotp">
+                    <button @click="validateTotp()">Submit TOTP Validation</button>
+                </div>
+                <br>
+                <button type="button" class="btn btn-danger m-3" @click="purgeAccount()">PURGE ACCOUNT</button>
+            </div>
+      </div>
     </div>
-    <br>
-    <button type="button" class="btn btn-danger m-3" @click="purgeAccount()">PURGE ACCOUNT</button>
 </div>
 
 
