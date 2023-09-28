459461727de80de85b4a9c684f718cf7e9f08fe3
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-09-27 22:24:21 -0400

    added route protection

--- server/routes/getRouter.js
+++ server/routes/getRouter.js
@@ -8,7 +8,7 @@ const initGetRouter = (app) => {
     getRouter.get('/api/get/user-profile', auth.isAuthenticated, getController.getUser)
     getRouter.get("/api/get/posts", getController.getPosts)
     getRouter.get("/api/get/post/:postID", getController.getPost)
-    getRouter.get("/api/get/users", getController.getUsers)
+    getRouter.get("/api/get/users", auth.isAuthenticated, getController.getUsers)
     getRouter.get("/api/enroll-mfa", auth.isAuthenticated, getController.enrollMFA)
     
     return app.use('/', getRouter)
